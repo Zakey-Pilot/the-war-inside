4a9f41026f47d98ee2464ee47e6aa142fce13db3
MohammedShuqair <eng.mohammed.shuqair@gmail.com>
2025-07-26 19:53:20 +0300

    fix: navbar position absolute

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar({ title }) {
             {menuOpen && (
                 <div className="fixed inset-0 z-40 bg-transparent md:hidden" onClick={() => setMenuOpen(false)}></div>
             )}
-            <div className="fixed top-0 left-0 w-full bg-black/80 py-3.5 px-4 md:px-32 flex justify-between items-center z-50">
+            <div className="absolute top-0 left-0 w-full bg-black/80 py-3.5 px-4 md:px-32 flex justify-between items-center z-50">
                 <Logo />
                 {/* Desktop Nav */}
                 <div className="hidden md:flex items-center gap-8">
@@ -35,7 +35,7 @@ export default function Navbar({ title }) {
             </div>
             {/* Slide-down Mobile Menu */}
             <div
-                className={`fixed top-0 left-0 w-full max-h-[60vh] bg-black z-50 transform transition-transform duration-300 md:hidden ${menuOpen ? 'translate-y-0' : '-translate-y-full'}`}
+                className={`absolute top-0 left-0 w-full max-h-[60vh] bg-black z-50 transform transition-transform duration-300 md:hidden ${menuOpen ? 'translate-y-0' : '-translate-y-full'}`}
                 style={{ boxShadow: menuOpen ? '0 2px 8px rgba(0,0,0,0.2)' : 'none' }}
             >
                 <div className="relative flex flex-col h-full p-8 gap-8">
